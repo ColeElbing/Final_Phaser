07f23964c0b1d434e30cbe87ab25598921aaf42d
Cole Elbing <8517401@isd535.org>
2022-06-01 21:50:48 -0500

    created the ability to fire based on where the mouse pointer is

--- scripts/evasion.js
+++ scripts/evasion.js
@@ -16,18 +16,23 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
-var bg;
-var ship;
-var bullets;
-var lastFired = 0;
-var cursors;
-var fire;
+let game = new Phaser.Game(config);
+let bg;
+let ship;
+let bullet;
+let cursors;
+let fire;
+let stats;
+let lastFired = 0;
+let isDown = false;
+let mouseX = 0;
+let mouseY = 0;
 
 function preload(){
     this.load.image('ship1','assets/thrust_ship.png');
     this.load.image('ship2','assets/thrust_ship2.png');
     this.load.image('background','assets/space.jpg');
+    this.load.image('bullet', 'assets/bullet1.png')
 }
 
 function create(){
@@ -47,11 +52,93 @@ function create(){
     keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-           
+    
+    //------------------Bullet firing code------------------
+    var Bullet = new Phaser.Class({
+
+        Extends: Phaser.GameObjects.Image,
+
+        initialize:
+
+        function Bullet (scene)
+        {
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+
+            this.incX = 0;
+            this.incY = 0;
+            this.lifespan = 0;
+
+            this.speed = Phaser.Math.GetSpeed(600, 1);
+        },
+
+        fire: function (x, y)
+        {
+            this.setActive(true);
+            this.setVisible(true);
+
+            //  Bullets fire from the middle of the screen to the given x/y
+            this.setPosition(ship.x, ship.y);
+
+            var angle = Phaser.Math.Angle.Between(x, y, ship.x, ship.y);
+
+            this.setRotation(angle);
+
+            this.incX = Math.cos(angle);
+            this.incY = Math.sin(angle);
+
+            this.lifespan = 1000;
+        },
+
+        update: function (time, delta)
+        {
+            this.lifespan -= delta;
+
+            this.x -= this.incX * (this.speed * delta);
+            this.y -= this.incY * (this.speed * delta);
+
+            if (this.lifespan <= 0)
+            {
+                this.setActive(false);
+                this.setVisible(false);
+            }
+        }
+
+    });
+
+    bullets = this.add.group({
+        classType: Bullet,
+        maxSize: 50,
+        runChildUpdate: true
+    });
+
+
+    //------------------End bullet firing code------------------
+
+
+    this.input.on('pointerdown', function (pointer) {
+
+        isDown = true;
+        mouseX = pointer.x;
+        mouseY = pointer.y;
+
+    });
+
+    this.input.on('pointermove', function (pointer) {
+
+        mouseX = pointer.x;
+        mouseY = pointer.y;
+
+    });
+
+    this.input.on('pointerup', function (pointer) {
+
+        isDown = false;
+
+    });
 }
 
-function update(){
-       if (keyA.isDown)
+function update(time, delta){
+    if (keyA.isDown)
     {
         ship.setAngularVelocity(-150);
     }
@@ -72,5 +159,17 @@ function update(){
     else
     {
         ship.setAcceleration(0);
+    }
+
+    if (isDown && time > lastFired)
+    {
+        bullet = bullets.get();
+
+        if (bullet)
+        {
+            bullet.fire(mouseX, mouseY);
+
+            lastFired = time + 50;
+        }
     }
 }
